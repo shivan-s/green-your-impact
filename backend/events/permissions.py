22525227661341d3be62bf6c68667514c82216e8
ShivanS93 <shivan.sivakumaran@gmail.com>
2021-09-01 17:45:02 +1200

    edited more tests, all done, but delete events to go

--- backend/events/permissions.py
+++ backend/events/permissions.py
@@ -1,24 +1,29 @@
 from rest_framework import permissions
 
 
-class IsOwnerAndPrivateEvent(permissions.BasePermission):
+class IsOwner(permissions.BasePermission):
     """
     Custom permission to allow users to see own private events
     """
 
-    message = "You do not have permission to view this event"
+    message = "You do not have permission to edit/delete this  event"
 
     def has_object_permission(self, request, view, obj):
         """
-        Owner of event can see/edit/delete their event
+        Owner can only edit/delete the event
         """
-        __import__("pdb").set_trace()
-        if obj.is_private == False:
-            # Public Events
-            return True
-        elif obj.is_private == True and obj.custom_user == request.user.id:
-            # Grant access since owner
-            return True
+        if request.method in permissions.SAFE_METHODS:
+            # private events should be stopped at the level of the query but
+            # this is here for extra security
+            if obj.is_private == True:
+                if obj.custom_user == request.user:
+                    return True
+                else:
+                    return False
+            else:
+                return True
         else:
-            # Decline access to private events
-            return False
+            if obj.custom_user == request.user:
+                return True
+            else:
+                return False
--- backend/events/tests.py
+++ backend/events/tests.py
@@ -6,7 +6,8 @@ import pytest
 from users.models import CustomUser
 from events.models import Event
 
-
+# TODO: Date range?
+# TODO: Set and Tear down isn't necessary
 class TestSetUp(APITestCase):
     """
     Setting up 2 test users each with 1 event. 1 event is public and 1 is private
@@ -63,11 +64,11 @@ class TestSetUp(APITestCase):
 
 
 # TODO: Build more tests: retrieve, edit, delete - also reorder
-# TODO reorder list, detail, create, edit, delete
+# TODO reorder list, detail/retrieve, create, edit, delete
 class TestEvent(TestSetUp):
-    def test_create_event(self):
+    def test_create_event_unauthenticated(self):
         """
-        Ensure event can only be created by an authenticated user
+        Ensure an unauthenticated user cannot create an event
         """
         # https://www.django-rest-framework.org/api-guide/routers/
         # on reverse names
@@ -78,103 +79,196 @@ class TestEvent(TestSetUp):
             "distance_travelled": 10.00,
             "description": "Test",
         }
-
-        # Unauthenticated user cannot create an event
         request = self.client.post(url, data, format="json")
+        filtered_query = Event.objects.filter(id=request.data.get("id"))
         assert request.status_code == status.HTTP_403_FORBIDDEN
+        assert filtered_query.count() == 0
 
-        # Authenticated user can create an event
+    def test_create_event_authenticated(self):
+        """
+        Ensure event can only be created by an authenticated user
+        """
+        url = reverse("events-list")
+        data = {
+            "is_private": False,
+            "transport_type": "BIKE",
+            "distance_travelled": 10.00,
+            "description": "Test",
+        }
         self.client.force_authenticate(user=self.user_1)
         request = self.client.post(url, data, format="json")
-        assert request.status_code == status.HTTP_201_CREATED
         filtered_query = Event.objects.filter(id=request.data["id"])
+        assert request.status_code == status.HTTP_201_CREATED
         assert filtered_query.count() == 1
         assert filtered_query[0].description == data["description"]
 
-    def test_list_event(self):
+    def test_list_event_public_unauthenticated(self):
         """
-        Ensure public events can be viewed and not private events unless by the owner
+        Ensure public events can be viewed by an unauthenticated user
         """
         url = reverse("events-list")
-
-        # Unauthenticated user can see public events and not private events
         request = self.client.get(url)
         assert request.status_code == status.HTTP_200_OK
         assert len(request.data) == 1
         assert request.data[0]["description"] == self.event_2_public.description
 
-        # Authenticated user 1 can see their own private event
+    def test_list_event_authenticated_user_1(self):
+        """
+        Authenticated user 1 can see their own private event and public events
+        """
+        url = reverse("events-list")
         self.client.force_authenticate(user=self.user_1)
         request = self.client.get(url)
-        assert request.status_code == status.HTTP_200_OK
-        assert len(request.data) == 2
         # set is used to ignore order
         user_1_description_set = {obj["description"] for obj in request.data}
+        assert request.status_code == status.HTTP_200_OK
+        assert len(request.data) == 2
         assert user_1_description_set == {
             self.event_2_public.description,
             self.event_1_private.description,
         }
 
-        # Authenticated user 2 cannot see user 1's private events
+    def test_list_event_authenticated_user_2(self):
+        """
+        Authenticated user 2 cannot see user 1's private events but can see public events
+        """
+        url = reverse("events-list")
         self.client.force_authenticate(user=self.user_2)
         request = self.client.get(url)
+        user_2_description_set = {obj["description"] for obj in request.data}
         assert request.status_code == status.HTTP_200_OK
         assert len(request.data) == 1
-        user_2_description_set = {obj["description"] for obj in request.data}
         assert user_2_description_set != {
             self.event_2_public.description,
             self.event_1_private.description,
         }
         assert request.data[0]["description"] == self.event_2_public.description
 
-    def test_retrieve_event(self):
+    def test_retrieve_event_unauthenticated_public(self):
         """
-        Ensure public events can be viewed and not private events unless by the owners
+        Ensure public events are visible to an unauthenticated user
         """
-        # Unauthenticated user can see public events and not private events
-        # - public event
         url = reverse("events-detail", args=[self.event_2_public.id])
         request = self.client.get(url)
         assert request.status_code == status.HTTP_200_OK
         assert request.data["description"] == self.event_2_public.description
-        # - private event
+
+    def test_retrieve_event_unauthenticated_private(self):
         url = reverse("events-detail", args=[self.event_1_private.id])
         request = self.client.get(url)
         # private event is not forbidden, it is hidden due to search query nature
         assert request.status_code == status.HTTP_404_NOT_FOUND
 
-        # Authenticated user 1 can see their own private event
+    def test_retrieve_event_user_1_private(self):
+        """
+        Ensure User 1 can see their own private event
+        """
         self.client.force_authenticate(user=self.user_1)
-        # - user 1 private event
         url = reverse("events-detail", args=[self.event_1_private.id])
         request = self.client.get(url)
         assert request.status_code == status.HTTP_200_OK
         assert request.data["description"] == self.event_1_private.description
 
-        # - public event
+    def test_retrieve_event_user_1_public(self):
+        """
+        Ensure User 1 can see User 2's public event
+        """
+        self.client.force_authenticate(user=self.user_1)
         url = reverse("events-detail", args=[self.event_2_public.id])
         request = self.client.get(url)
         assert request.status_code == status.HTTP_200_OK
         assert request.data["description"] == self.event_2_public.description
 
-        # Authenticated user 2 cannot see user 1's private events
+    def test_retrieve_event_user_2_private(self):
+        """
+        Ensure User 2 cannot see User 1's private event
+        """
         self.client.force_authenticate(user=self.user_2)
-        # - user 1 private event
         url = reverse("events-detail", args=[self.event_1_private.id])
         request = self.client.get(url)
         assert request.status_code == status.HTTP_404_NOT_FOUND
 
-        # - public event
+    def test_retrieve_event_user_2_public(self):
+        """
+        Ensure User 2 cannot their own public event
+        """
+        self.client.force_authenticate(user=self.user_2)
         url = reverse("events-detail", args=[self.event_2_public.id])
         request = self.client.get(url)
         assert request.status_code == status.HTTP_200_OK
         assert request.data["description"] == self.event_2_public.description
 
-    def test_edit_event(self):
+    def test_edit_event_unauthenticated_public(self):
         """
-        Ensure event can only be edited by the owner
+        Unauthenicated user cannot edit a public event
         """
-        pass
+        data = {
+            "is_private": False,
+            "transport_type": "BIKE",
+            "distance_travelled": 10.00,
+            "description": "Test - Change",
+        }
+        url = reverse("events-detail", args=[self.event_2_public.id])
+        request = self.client.put(url, data, format="json")
+        filtered_query = Event.objects.filter(id=self.event_2_public.id)
+        assert request.status_code == status.HTTP_403_FORBIDDEN
+        assert filtered_query[0].description != data["description"]
+
+    def test_edit_event_unauthenticated_private(self):
+        """
+        Unauthenticated user cannot edit a private event
+        """
+        data = {
+            "is_private": False,
+            "transport_type": "BIKE",
+            "distance_travelled": 10.00,
+            "description": "Test - Change",
+        }
+        url = reverse("events-detail", args=[self.event_1_private.id])
+        request = self.client.get(url, data, format="json")
+        filtered_query = Event.objects.filter(id=self.event_1_private.id)
+        # 404 instead of 403 since query does not show private events
+        assert request.status_code == status.HTTP_404_NOT_FOUND
+        assert filtered_query[0].description != data["description"]
+
+    def test_edit_event_user_1_own_and_change_privacy(self):
+        """
+        User 1 can edit their own event and can change privacy from private to public
+        """
+        data = {
+            "is_private": False,
+            "transport_type": "BIKE",
+            "distance_travelled": 10.00,
+            "description": "Test - Change",
+        }
+        url = reverse("events-detail", args=[self.event_1_private.id])
+        self.client.force_authenticate(user=self.user_1)
+        request = self.client.put(url, data, format="json")
+        filtered_query = Event.objects.filter(id=self.event_1_private.id)
+        assert request.status_code == status.HTTP_200_OK
+        assert filtered_query[0].description == data["description"]
+        # check public with unauthenticated user
+        self.client.force_authenticate(user=None)
+        self.client.get(url)
+        assert request.status_code == status.HTTP_200_OK
+        assert request.data["description"] == data["description"]
+
+    def test_edit_event_user_1_other(self):
+        """
+        User 1 cannot edit User 2's event
+        """
+        data = {
+            "is_private": True,
+            "transport_type": "BIKE",
+            "distance_travelled": 10.00,
+            "description": "Test - Change",
+        }
+        url = reverse("events-detail", args=[self.event_2_public.id])
+        self.client.force_authenticate(user=self.user_1)
+        request = self.client.put(url, data, format="json")
+        filtered_query = Event.objects.filter(id=self.event_2_public.id)
+        assert request.status_code == status.HTTP_403_FORBIDDEN
+        assert filtered_query[0].description != data["description"]
 
     def test_delete_event(self):
         """
