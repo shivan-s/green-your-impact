74844d23c547bfc359e9239209b828d8e96a41c1
ShivanS93 <shivan.sivakumaran@gmail.com>
2021-09-09 22:55:11 +1200

    updating

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,19 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import React, { setState, useEffect } from "react";
+// import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
-class App extends Component {
-	state = {
-		events: [],
-	};
+function App() {
+	const [data, setData] = setState(false);
+	useEffect(() => {
+		fetch(`http://127.0.0.1:8000/api/v1/events/events`)
+			.then((response) => response.json())
+			.then(setData);
+	}, []);
+	console.log(data);
 
-	async componentDidMount() {
-		try {
-			const res = await fetch("http://127.0.0.1:8000/api/v1/events/events");
-			const events = await res.json();
-			this.setState({
-				events,
-			});
-		} catch (e) {
-			console.log(e);
-		}
+	if (data) {
+		return <div className="container">{data}</div>;
 	}
-
-	render() {
-		return (
-			<div className="container">
-				{this.state.events.map((one_event) => (
-					<div key={one_event.id}>
-						<h1>
-							<a href={one_event.custom_user}>{one_event.custom_user_name}</a>
-						</h1>
-						<p>
-							{one_event.transport_type} - {one_event.distance_travelled}
-						</p>
-						<p>{one_event.description}</p>
-					</div>
-				))}
-			</div>
-		);
-	}
+	return <div>...loading</div>;
 }
 
 export default App;
---
+++ frontend/src/pages.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export function Home() {
+	return (
+		<div>
+			<H1>[Your Events]</H1>
+		</div>
+	);
+}
+export function Users() {
+	return (
+		<div>
+			<H1>[Your Events]</H1>
+		</div>
+	);
+}
